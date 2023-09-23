644e8ff704b0d430d9697cec99905c97881e104a
RitikRaj-1 <145713391+RitikRaj-1@users.noreply.github.com>
2023-09-24 03:06:25 +0530

    Assignment_2
    This is the second assignment for Namaste react course.

---
+++ Assignment_2/App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1",{id : "heading"},"Hello World from React!")
+
+// const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+// root.render(heading)
+
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+const heading = React.createElement("div",{id : "parent"},
+                                    [React.createElement("div",{id:"child1"},
+                                        React.createElement("h1",{},"I'm an h1 tag.")),
+                                    React.createElement("div",{id:"child2"},
+                                        React.createElement("h1",{},"I'm an h2 tag."))]);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(heading)
